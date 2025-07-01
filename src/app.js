9b571375806e7a4d80c8150ba3d557e252f5f2e0
Fvazquez <foapgenerico@365.stucom.com>
2025-07-01 17:14:46 +0200

    stucom

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ import express from "express";
 import path from "node:path";
 const app = express();
 
+import {connection} from "../mysql/mysql.js";
+
+
 process.loadEnvFile();
 const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-    res.send("Todo OK");
+    const query = "SELECT * FROM pelis";
+    connection.query(query, (err, result, fields) => {
+        if (err) throw err;
+        res.json(result)
+    })
+
 });
 
 app.listen(PORT, () => {console.log(`Servidor levantado en http://localhost:${PORT}`)})
