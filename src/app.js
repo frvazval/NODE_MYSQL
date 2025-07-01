ff3defbe27b8b64c1207f0c6c2842852dd7a913e
Fvazquez <foapgenerico@365.stucom.com>
2025-07-01 18:14:37 +0200

    stucom

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 import express from "express";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 // Conecta con la base de datos
 import {connection} from "../mysql/mysql.js";
 
+
 process.loadEnvFile();
 const PORT = process.env.PORT || 3000;
 
 // Ruta de los ficheros estaticos (css, javascript, etc...)
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 // Ruta de las plantillas
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "..", "views"));
 
 app.get("/", (req, res) => {
     const query = "SELECT * FROM pelis";
