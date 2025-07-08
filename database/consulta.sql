62936f95e1fc1d7f2276751c8d116f031d26ed31
Fvazquez <foapgenerico@365.stucom.com>
2025-07-08 16:17:04 +0200

    stucom

--- database/consulta.sql
+++ database/consulta.sql
@@ -1,4 +1,4 @@
-SELECT d.nombre_director, g.nombre_genero, p.titulo_peli, p.fecha, p.imagen_peli, p.valoracion
+SELECT d.nombre_director, g.nombre_genero, p.titulo_peli, p.fecha, p.imagen_peli, p.valoracion, p.sinopsis
 FROM directores d
 NATURAL JOIN pelis p
 NATURAL JOIN pelis_generos pg
--- src/app.js
+++ src/app.js
@@ -34,8 +34,21 @@ app.get("/", (req, res) => {
 
 app.get("/peli/:id", (req, res) => {
     const id = req.params.id;
-    // res.send("ok");
-    res.render("peli")
+   
+    const query = `SELECT d.nombre_director, g.nombre_genero, p.titulo_peli, p.fecha, p.imagen_peli, p.valoracion, p.sinopsis
+                    FROM directores d
+                    NATURAL JOIN pelis p
+                    NATURAL JOIN pelis_generos pg
+                    NATURAL JOIN generos g
+                    WHERE p.id_peli = ${id}`;
+
+    connection.query(query, (err, result, fields) => {
+        if (err) throw err;        
+
+        res.render("peli", {result});
+    });
+    
+    
 })
 
 app.listen(PORT, () => {console.log(`Servidor levantado en http://localhost:${PORT}`)})
